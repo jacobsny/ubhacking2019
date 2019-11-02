0104496f0908103baad354af19bc45dc733c12e2
jacobsny <jacob.snyderman@gmail.com>
2019-11-02 14:47:11 -0400

    initial comments layed out

--- src/backend/backend.py
+++ src/backend/backend.py
@@ -1,12 +1,18 @@
-def main():
+def processIMG(imgArr):
+    # this function takes a numpy array
+    # passed to Google Cloud Vision API
+    # processes OCR data
+    # returns string in code formatting written
+
+def processSyntax(flags, string, img):
+    # this function takes the formatted string from processIMG
+    # it takes the flags where there are syntax errors
+    # it outputs a numpy array of the original img with the
+    # new text data and flags overlayed on the img
+
+def main(imgArr):
     # this is where the main will go
-    # I need to have a function that takes in the image data as a 2d array
-    # that function will then use the google cloud api and will pass the image through OCR
-    # The OCR will return Google's custom format
-    # I will then post process that format into a properly formated text file with tabs and all
-    # I will pass the text file as a string to John's Piece of BackEnd
-    # John's BackEnd will return the a list of flags that the syntax checker gave us for that text file
-    # I will then take the text file and the flags and will format them into a canvas
-    # this canvas will then be overlayed on the actual image from the display.
-    # the goal is to keep this live but if it's a take a photo and it then just overlays the photo
-    # that will most likely be acceptable
+    # processIMG will be called on imgARR
+    # returned data will go to John T
+    # img numpy array, text string, and flags will be passed to processSyntax
+    # returned img will be sent to server
