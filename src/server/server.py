4ed55ee690460349588b2ae115b0c9a4d35b8884
jmacias11 <43096644+jmacias11@users.noreply.github.com>
2019-11-02 23:50:28 -0400

    FUCKING SOCKETS TAKE BYTES

--- src/server/server.py
+++ src/server/server.py
@@ -1,7 +1,6 @@
 
 from flask import Flask
 from flask_socketio import SocketIO, emit
-from PIL import Image, ImageDraw
 
 import base64
 import io
@@ -9,22 +8,25 @@ import json
 import base64
 
 
-#app = Flask(__name__)
-#app.config['SECRET_KEY'] = "whatamievendoing"
-#socketio = SocketIO(app)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "whatamievendoing"
+socketio = SocketIO(app)
 
-#@socketio.on('toServer')
+@socketio.on('toServer')
 def handleImage(imgRaw):
 
-    x = base64.b64decode(imgRaw)
-    stream = io.BytesIO(x)
-    image = Image.open(stream)
-    image.save("test.png", "PNG")
+    encodedImage = base64.b64encode(imgRaw)
 
-with open("download.jpg","rb") as img:
-    x = img.read()
-    y = base64.b64encode(x)
-    handleImage(y)
+    return encodedImage
+
+
+def sendImage(encodedImage):
+
+    decodedImage = base64.b64decode(encodedImage)
+
+    return decodedImage
+
+
 
 
 
